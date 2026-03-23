51101a3d7b65ceda84a4bd790f7cab04f7a10393
emichael <emichael@users.noreply.github.com>
2026-03-23 14:46:24 +0000

    Committing built handout

---
+++ labs/lab1-clientserver/src/dslabs/clientserver/SimpleClient.java
@@ -0,0 +1,69 @@
+package dslabs.clientserver;
+
+import dslabs.framework.Address;
+import dslabs.framework.Client;
+import dslabs.framework.Command;
+import dslabs.framework.Node;
+import dslabs.framework.Result;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Simple client that sends requests to a single server and returns responses.
+ *
+ * <p>See the documentation of {@link Client} and {@link Node} for important implementation notes.
+ */
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+class SimpleClient extends Node implements Client {
+  private final Address serverAddress;
+
+  // Your code here...
+
+  /* -----------------------------------------------------------------------------------------------
+   *  Construction and Initialization
+   * ---------------------------------------------------------------------------------------------*/
+  public SimpleClient(Address address, Address serverAddress) {
+    super(address);
+    this.serverAddress = serverAddress;
+  }
+
+  @Override
+  public synchronized void init() {
+    // No initialization necessary
+  }
+
+  /* -----------------------------------------------------------------------------------------------
+   *  Client Methods
+   * ---------------------------------------------------------------------------------------------*/
+  @Override
+  public synchronized void sendCommand(Command command) {
+    // Your code here...
+  }
+
+  @Override
+  public synchronized boolean hasResult() {
+    // Your code here...
+    return false;
+  }
+
+  @Override
+  public synchronized Result getResult() throws InterruptedException {
+    // Your code here...
+    return null;
+  }
+
+  /* -----------------------------------------------------------------------------------------------
+   *  Message Handlers
+   * ---------------------------------------------------------------------------------------------*/
+  private synchronized void handleReply(Reply m, Address sender) {
+    // Your code here...
+  }
+
+  /* -----------------------------------------------------------------------------------------------
+   *  Timer Handlers
+   * ---------------------------------------------------------------------------------------------*/
+  private synchronized void onClientTimer(ClientTimer t) {
+    // Your code here...
+  }
+}
---
+++ labs/lab2-primarybackup/src/dslabs/primarybackup/PBClient.java
@@ -0,0 +1,70 @@
+package dslabs.primarybackup;
+
+import dslabs.framework.Address;
+import dslabs.framework.Client;
+import dslabs.framework.Command;
+import dslabs.framework.Node;
+import dslabs.framework.Result;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+class PBClient extends Node implements Client {
+  private final Address viewServer;
+
+  // Your code here...
+
+  /* -----------------------------------------------------------------------------------------------
+   *  Construction and Initialization
+   * ---------------------------------------------------------------------------------------------*/
+  public PBClient(Address address, Address viewServer) {
+    super(address);
+    this.viewServer = viewServer;
+  }
+
+  @Override
+  public synchronized void init() {
+    // Your code here...
+  }
+
+  /* -----------------------------------------------------------------------------------------------
+   *  Client Methods
+   * ---------------------------------------------------------------------------------------------*/
+  @Override
+  public synchronized void sendCommand(Command command) {
+    // Your code here...
+  }
+
+  @Override
+  public synchronized boolean hasResult() {
+    // Your code here...
+    return false;
+  }
+
+  @Override
+  public synchronized Result getResult() throws InterruptedException {
+    // Your code here...
+    return null;
+  }
+
+  /* -----------------------------------------------------------------------------------------------
+   *  Message Handlers
+   * ---------------------------------------------------------------------------------------------*/
+  private synchronized void handleReply(Reply m, Address sender) {
+    // Your code here...
+  }
+
+  private synchronized void handleViewReply(ViewReply m, Address sender) {
+    // Your code here...
+  }
+
+  // Your code here...
+
+  /* -----------------------------------------------------------------------------------------------
+   *  Timer Handlers
+   * ---------------------------------------------------------------------------------------------*/
+  private synchronized void onClientTimer(ClientTimer t) {
+    // Your code here...
+  }
+}
